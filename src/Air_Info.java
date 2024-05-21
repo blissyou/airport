276b4865a99f2a7c7a1a58759fde2e3d8d08e70e
abulleDev <olives2739@gmail.com>
2024-05-21 19:38:57 +0900

    add getter setter

--- src/Air_Info.java
+++ src/Air_Info.java
@@ -19,6 +19,53 @@ public class Air_Info {
         this.Desti = Desti;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public int getGate() {
+        return gate;
+    }
+
+    public String getD_Time() {
+        return D_Time;
+    }
+
+    public String getA_Time() {
+        return A_Time;
+    }
+
+    public String getDesti() {
+        return Desti;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public void setGate(int gate) {
+        this.gate = gate;
+    }
+
+    public void setD_Time(String d_Time) {
+        D_Time = d_Time;
+    }
+
+    public void setA_Time(String a_Time) {
+        A_Time = a_Time;
+    }
+
+    public void setDesti(String desti) {
+        Desti = desti;
+    }
 
     public String toString(){
         return "항공사 : " + name + "\t" + "날짜 : " + day + "\t" + "\t" + "게이트 : " + gate + "\n" + "출발 시간 : " + D_Time + "\t" + "도착시간 : " + A_Time + "\t" + "목적지 : " + Desti;
